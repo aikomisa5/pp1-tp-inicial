a62e545d59bc4328eeab5ac88b2749ed02d4ea3a
roberGzt <rober.gerszen@gmail.com>
2017-08-20 19:26:01 -0300

    añadidos métodos de daos
    daoLocalidad
    daoTipoDeContacto

--- Agenda/src/modelo/Agenda.java
+++ Agenda/src/modelo/Agenda.java
@@ -1,15 +1,26 @@
 package modelo;
 
 import java.util.List;
+
+import dto.LocalidadDTO;
 import dto.PersonaDTO;
+import dto.TipoDeContactoDTO;
+import persistencia.dao.interfaz.LocalidadDAO;
 import persistencia.dao.interfaz.PersonaDAO;
+import persistencia.dao.interfaz.TipoDeContactoDAO;
+import persistencia.dao.mysql.LocalidadDAOImpl;
 import persistencia.dao.mysql.PersonaDAOImpl;
+import persistencia.dao.mysql.TipoDeContactoDAOImpl;
 
 public class Agenda {
 	private PersonaDAO persona;
+	private LocalidadDAO localidad;
+	private TipoDeContactoDAO tipoDeContacto;
 
 	public Agenda() {
 		persona = new PersonaDAOImpl();
+		localidad = new LocalidadDAOImpl();
+		tipoDeContacto = new TipoDeContactoDAOImpl();
 	}
 
 	public void agregarPersona(PersonaDTO nuevaPersona) {
@@ -27,5 +38,14 @@ public class Agenda {
 	public void modificarPersona(PersonaDTO personaAModificar) {
 		persona.update(personaAModificar);
 	}
+
+	public List<LocalidadDTO> getLocalidades() {
+		return localidad.readAll(); 
+	}
+
+	public List<TipoDeContactoDTO> getTiposDeContacto() {
+		return tipoDeContacto.readAll();
+	}
+	
 
 }
