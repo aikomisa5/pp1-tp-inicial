8d0bb9d2aba3a14bc9b3a664c4f68a6159cd5ebc
roberGzt <rober.gerszen@gmail.com>
2017-08-20 17:36:56 -0300

    modificado metodo LlenarTabla
    Carga el modelo completo en la vista principal.

--- Agenda/src/presentacion/controlador/Controlador.java
+++ Agenda/src/presentacion/controlador/Controlador.java
@@ -2,90 +2,72 @@ package presentacion.controlador;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.Date;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
 import java.util.List;
 import modelo.Agenda;
 import presentacion.reportes.ReporteAgenda;
 import presentacion.vista.VentanaPersona;
 import presentacion.vista.Vista;
-import dto.DomicilioDTO;
-import dto.LocalidadDTO;
 import dto.PersonaDTO;
 
-public class Controlador implements ActionListener
-{
-		private Vista vista;
-		private List<PersonaDTO> personas_en_tabla;
-		private VentanaPersona ventanaPersona; 
-		private Agenda agenda;
-		
-		public Controlador(Vista vista, Agenda agenda)
-		{
-			this.vista = vista;
-			this.vista.getBtnAgregar().addActionListener(this);
-			this.vista.getBtnBorrar().addActionListener(this);
-			this.vista.getBtnReporte().addActionListener(this);
-			this.agenda = agenda;
-			this.personas_en_tabla = null;
-		}
-		
-		public void inicializar()
-		{
+public class Controlador implements ActionListener {
+	private Vista vista;
+	private List<PersonaDTO> personasEnTabla;
+	private VentanaPersona ventanaPersona;
+	private Agenda agenda;
+
+	public Controlador(Vista vista, Agenda agenda) {
+		this.vista = vista;
+		this.vista.getBtnAgregar().addActionListener(this);
+		this.vista.getBtnBorrar().addActionListener(this);
+		this.vista.getBtnReporte().addActionListener(this);
+		this.agenda = agenda;
+		this.personasEnTabla = null;
+	}
+
+	public void inicializar() {
+		this.llenarTabla();
+	}
+
+	private void llenarTabla() {
+		this.vista.getModelPersonas().setRowCount(0); // Para vaciar la tabla
+		this.vista.getModelPersonas().setColumnCount(0);
+		this.vista.getModelPersonas().setColumnIdentifiers(this.vista.getNombreColumnas());
+		this.personasEnTabla = agenda.obtenerPersonas();
+		personasEnTabla.forEach(p -> {
+			Object[] fila = { p.getNombre(), p.getTelefono(), p.getDomicilio().getCalle(), p.getDomicilio().getAltura(),
+					p.getDomicilio().getPiso(), p.getDomicilio().getDepartamento(),
+					p.getDomicilio().getLocalidad().getNombre(), p.getMail(), p.getFechaCumpleaños(),
+					p.getTipoDeContacto().getNombre() };
+			vista.getModelPersonas().addRow(fila);
+		});
+		this.vista.show();
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == this.vista.getBtnAgregar()) {
+			this.ventanaPersona = new VentanaPersona(this);
+			this.ventanaPersona.setVisible(true);
+		} else if (e.getSource() == this.vista.getBtnBorrar()) {
+			int[] filas_seleccionadas = this.vista.getTablaPersonas().getSelectedRows();
+			for (int fila : filas_seleccionadas) {
+				this.agenda.borrarPersona(this.personasEnTabla.get(fila));
+			}
+
 			this.llenarTabla();
+
+		} else if (e.getSource() == this.vista.getBtnReporte()) {
+			ReporteAgenda reporte = new ReporteAgenda(agenda.obtenerPersonas());
+			reporte.mostrar();
+		} else if (e.getSource() == this.ventanaPersona.getBtnAgregarPersona()) {
+			this.llenarTabla();
+			this.ventanaPersona.dispose();
+		} else if (e.getSource() == this.vista.getBtnEditar()) {
+			PersonaDTO nuevaPersona = new PersonaDTO(0, this.ventanaPersona.getTfNombre().getText(),
+					ventanaPersona.getTfTelefono().getText());
+			this.agenda.agregarPersona(nuevaPersona);
+			this.llenarTabla();
+			this.ventanaPersona.dispose();
 		}
-		
-		private void llenarTabla()
-		{
-			this.vista.getModelPersonas().setRowCount(0); //Para vaciar la tabla
-			this.vista.getModelPersonas().setColumnCount(0);
-			this.vista.getModelPersonas().setColumnIdentifiers(this.vista.getNombreColumnas());
-			
-			this.personas_en_tabla = agenda.obtenerPersonas();
-			for (int i = 0; i < this.personas_en_tabla.size(); i ++)
-			{
-				Object[] fila = {this.personas_en_tabla.get(i).getNombre(), this.personas_en_tabla.get(i).getTelefono()};
-				this.vista.getModelPersonas().addRow(fila);
-			}
-			this.vista.show();
-		}
-		
-		public void actionPerformed(ActionEvent e) 
-		{
-			if(e.getSource() == this.vista.getBtnAgregar())
-			{
-				this.ventanaPersona = new VentanaPersona(this);
-			}
-			else if(e.getSource() == this.vista.getBtnBorrar())
-			{
-				int[] filas_seleccionadas = this.vista.getTablaPersonas().getSelectedRows();
-				for (int fila:filas_seleccionadas)
-				{
-					this.agenda.borrarPersona(this.personas_en_tabla.get(fila));
-				}
-				
-				this.llenarTabla();
-				
-			}
-			else if(e.getSource() == this.vista.getBtnReporte())
-			{				
-				ReporteAgenda reporte = new ReporteAgenda(agenda.obtenerPersonas());
-				reporte.mostrar();				
-			}
-			else if(e.getSource() == this.ventanaPersona.getBtnAgregarPersona())
-			{				
-				this.llenarTabla();
-				this.ventanaPersona.dispose();
-			}
-			else if(e.getSource() == this.vista.getBtnEditar())
-			{
-				PersonaDTO nuevaPersona = new PersonaDTO(0,this.ventanaPersona.getTfNombre().getText(), ventanaPersona.getTfTelefono().getText());
-				this.agenda.agregarPersona(nuevaPersona);
-				this.llenarTabla();
-				this.ventanaPersona.dispose();
-			}
-		}
+	}
 
 }
