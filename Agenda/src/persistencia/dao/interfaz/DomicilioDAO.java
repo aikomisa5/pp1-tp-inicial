b302a84a5a501a8ad1a3e7da7d4cf9e57bd67fc9
deidelson <deidelson@mail.com>
2017-08-20 10:44:06 -0300

    agregado LocalidadDAO

--- Agenda/src/persistencia/dao/interfaz/DomicilioDAO.java
+++ Agenda/src/persistencia/dao/interfaz/DomicilioDAO.java
@@ -1,13 +1,18 @@
 package persistencia.dao.interfaz;
 
+import java.util.List;
+
 import dto.DomicilioDTO;
+import dto.LocalidadDTO;
 
 public interface DomicilioDAO {
 	
-	public int agregarDomicilio();
+	public int insert(DomicilioDTO domicilio);
 	
-	public boolean editarDomicilio(DomicilioDTO domicilio);
+	public boolean update(DomicilioDTO domicilio);
 		
-	public boolean borrarDomicilio(DomicilioDTO domicilio); 
+	public boolean delete(DomicilioDTO domicilio); 
+	
+	public List<LocalidadDTO> readAll();
 
 }
