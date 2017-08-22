215f8bb46a7e82a9b4a82f9c06a5e40742b54366
deidelson <deidelson@mail.com>
2017-08-22 12:49:38 -0300

    Nuevo script y clase conexion seteada

--- Agenda/sql/scriptAgenda.sql
+++ Agenda/sql/scriptAgenda.sql
@@ -1,29 +1,27 @@
-use agenda;
+create database if not exists tpi_g15;
+
+use tpi_g15;
 
 drop table if exists personas;
 drop table if exists tiposDeContacto; 
 drop table if exists domicilios;
 drop table if exists localidades;
 
-
-
-
-CREATE TABLE  tiposDeContacto
+CREATE TABLE if not exists tiposDeContacto
 (
 	idTipo int not null auto_increment,
 	nombreTipo varchar(30) not null unique,
     primary key (idTipo)
 );
 
-
-CREATE TABLE localidades
+CREATE TABLE if not exists localidades
 (
 	idLocalidad int not null auto_increment,
     nombreLocalidad varchar(45) not null,
     primary key (idLocalidad)
 );
 
-CREATE TABLE  domicilios
+CREATE TABLE if not exists domicilios
 (
 	idDomicilio int not null auto_increment,
 	calle varchar (45) not null,
@@ -35,7 +33,7 @@ CREATE TABLE  domicilios
     foreign key (idLocalidad) references localidades (idLocalidad)
 );
 
-CREATE TABLE  personas 
+CREATE TABLE if not exists personas 
 (
   idPersona int(11) NOT NULL AUTO_INCREMENT,
   Nombre varchar(45) NOT NULL,
@@ -51,8 +49,6 @@ CREATE TABLE  personas
 
 
 
-# comandos de insercion de datos, correrlos antes de correr el test!!
-
 insert into tiposDeContacto (nombreTipo) values ('Familia');
 insert into tiposDeContacto (nombreTipo) values ('Amigos');
 insert into tiposDeContacto (nombreTipo) values ('Trabajo');
@@ -65,24 +61,3 @@ insert into localidades (nombreLocalidad) values ('Tortuguitas');
 insert into localidades (nombreLocalidad) values ('Pablo Nogues');
 insert into localidades (nombreLocalidad) values ('Polvorines');
 insert into localidades (nombreLocalidad) values ('Don Tortcuato');
-
-insert into domicilios (calle, altura, piso, departamento, idLocalidad)
- values ('sarasa', 12312, 3, '4b', 1);
- 
- insert into personas (Nombre, telefono, mail, cumpleanios, idTipo, idDomicilio)
- values ('Mix 2.0', '123123123', 'ProfeDanielLover@gmail.com', '1995-02-04', 2, 1);
- 
- update tiposDeContacto
- set nombreTipo = "familias"
- where idTipo=1;
- 
- select * from tiposDeContacto;
- 
- 
- #Query de verificacion
- 
- select * from personas p 
- inner join domicilios d
- on p.idDomicilio = d.idDomicilio
- inner join localidades l
- on d.idLocalidad = l.idLocalidad;
