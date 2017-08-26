f97d75a78ff8c566bcc1d3dddf8ad40099a9faea
roberGzt <rober.gerszen@gmail.com>
2017-08-26 17:33:20 -0300

    Agregado generador de datos
    Ejecutar pruebasDate para agregar los datos.

--- Agenda/src/pruebas/pruebasDate.java
+++ Agenda/src/pruebas/pruebasDate.java
@@ -27,28 +27,28 @@ public class pruebasDate {
 //		date = Date.valueOf(dateJava.toString());
 //		System.out.println(date.toString());
 		
-		Date date1  = Date.valueOf("2017-02-06");
-		Date date2  = Date.valueOf("2019-02-05");
-		Date date3  = Date.valueOf("2019-02-06");
+//		Date date1  = Date.valueOf("2017-02-06");
+//		Date date2  = Date.valueOf("2019-02-05");
+//		Date date3  = Date.valueOf("2019-02-06");
+//		
+//		Calendar fecha1 = Calendar.getInstance();
+//		fecha1.setTime(date1);
+//		fecha1.set(Calendar.YEAR, 1000);
+//		
+//		Calendar fecha2 = Calendar.getInstance();
+//		fecha2.setTime(date2);
+//		fecha2.set(Calendar.YEAR, 1000);
+//		
+//		Calendar fecha3 = Calendar.getInstance();
+//		fecha3.setTime(date3);
+//		fecha3.set(Calendar.YEAR, 1000);
+//		
+//		System.out.println("Comparamos los dates : "+date1.compareTo(date2));
+//		System.out.println("Comparamos los calendars: "+fecha1.compareTo(fecha2));
+//		System.out.println("Comparamos 2 calendar iguales: "+fecha3.compareTo(fecha1)+" "+fecha1.compareTo(fecha3));
+//		System.out.println("DayOfMouth "+fecha1.get(Calendar.DAY_OF_MONTH));
 		
-		Calendar fecha1 = Calendar.getInstance();
-		fecha1.setTime(date1);
-		fecha1.set(Calendar.YEAR, 1000);
-		
-		Calendar fecha2 = Calendar.getInstance();
-		fecha2.setTime(date2);
-		fecha2.set(Calendar.YEAR, 1000);
-		
-		Calendar fecha3 = Calendar.getInstance();
-		fecha3.setTime(date3);
-		fecha3.set(Calendar.YEAR, 1000);
-		
-		System.out.println("Comparamos los dates : "+date1.compareTo(date2));
-		System.out.println("Comparamos los calendars: "+fecha1.compareTo(fecha2));
-		System.out.println("Comparamos 2 calendar iguales: "+fecha3.compareTo(fecha1)+" "+fecha1.compareTo(fecha3));
-		System.out.println("DayOfMouth "+fecha1.get(Calendar.DAY_OF_MONTH));
-		
-		
+		GeneradorDeDatos.generarDatos();
 		
 	}
 
