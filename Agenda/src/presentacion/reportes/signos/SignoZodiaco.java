eda7a721b0a1c6b8e4b2896af4900fddb8e848db
roberGzt <rober.gerszen@gmail.com>
2017-08-30 11:10:04 -0300

    override de método toString de enum SignoZodiaco

--- Agenda/src/presentacion/reportes/signos/SignoZodiaco.java
+++ Agenda/src/presentacion/reportes/signos/SignoZodiaco.java
@@ -2,6 +2,11 @@ package presentacion.reportes.signos;
 public enum SignoZodiaco {
 	CAPRICORNIO,ACUARIO,PISCIS,ARIES,TAURO,GÉMINIS,CÁNCER,LEO,VIRGO,
 	LIBRA,ESCORPIÓN,SAGITARIO;
-
+	
+	@Override
+	public String toString() {
+		String palabraFormateada =  super.toString().substring(0, 1) +super.toString().toLowerCase().substring(1);
+		return palabraFormateada;
+	}
 	}
 
